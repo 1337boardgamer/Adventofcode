4e8bbbcd9129b0b49540921ef705e00dd42e30a2
1337boardgamer <89208354+1337boardgamer@users.noreply.github.com>
2021-11-17 08:55:59 -0500

    Update temp.py

--- python/temp.py
+++ python/temp.py
@@ -1,5 +1,14 @@
 ##this an ability score roller
-import math
+import random
 def roll(s):
-  return math.random(s)
-print(roll(6))
+  return random.randint(1,s)
+def abilityscore():
+    total=low=temp=roll(6)
+    print(total)
+    for t in range(1,4,1):
+        temp=roll(6)
+        if temp<low:
+            low=temp
+        total+=temp
+    return total-low
+print(abilityscore())
